f9ab629598394c2391aa27389a4f09aa92835152
Marc Juchli <mail@marcjuch.li>
2017-09-14 23:07:58 +0200

    Clean up main

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ import reporter as r
 import strategy as s
 import trader as t
 
-stream = cs.CandleStream(m.GdaxCandle60, limit = 5000, size = 60)
+stream = cs.CandleStream(m.GdaxCandle15, size = 15, limit = 2000)
 executor = ex.ExecutorMock(crypto=0.0, fiat=1000.0, market="BTC/EUR")
-reporter = r.Reporter("trades-60.tsv")
+reporter = r.Reporter("trades-15.tsv")
 reporter.setup(crypto=0.0, fiat=1000.0)
 executor.setReporter(reporter)
 strategy = s.Strategy(executor)
 
 trader = t.Trader(stream, executor, strategy)
-trader.setup(retrainWait = 10, decisionWait = 30, LB = 100, testSize = 0.1, epochs = 100, batchSize = 200)
-#trader.backtest(plot = True)
-trader.run()
+trader.setup(retrainWait = 7,
+             decisionWait = 15,
+             LB = 15,
+             testSize = 0.1,
+             epochs = 100,
+             batchSize = 500)
 
-print reporter.getTrades()
-print executor.getAccountBalance()
+trader.backtest(plot = True, scaled = True)
+#trader.run()
+
+
+#print reporter.getTrades()
+#print executor.getAccountBalance()
